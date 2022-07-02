77afcccae9d57c3507efb74ca8bf41c564d2125f
DmitriyMalofeyev <et5@list.ru>
2022-07-03 00:16:44 +0300

    добавлено решение домашнего задания

---
+++ Task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+/*
+Console.WriteLine("Введите трехзначное число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int secondnumber = (numberA  / 10) % 10;
+Console.WriteLine ("Вторая цифра в нем " + secondnumber);
+*/
+
+
+int method1 (int start, int finish)
+{
+    int numberA = new Random().Next(start, finish);
+    Console.WriteLine ("Выбрано число " + numberA);
+    int secondnumber = numberA  / 10 % 10;
+    int result = secondnumber;
+    return result;
+}
+
+
+
+int A = method1 (100, 999);
+Console.WriteLine ("Вторая цифра в нем " + A);
---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void thirdnumber(int x)
+{
+    int div = 1;
+    while (x / div >= 10)
+    {
+        div *= 10;
+    }
+    if (div < 100)
+    {
+        Console.WriteLine ("Выбрано слишком короткое число");
+    }
+    else
+    {
+        int digit = (x / (div/100)) % 10;
+        Console.WriteLine("Третья цифра {0}", digit);
+    }
+}
+
+
+Console.WriteLine("Введите целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+thirdnumber(numberA);
+
+
+
+
